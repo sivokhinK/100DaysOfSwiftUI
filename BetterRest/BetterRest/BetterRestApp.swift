2d2212220ae86449563b5e643b3f85dd61fac0af
Kirill Sivokhin <kirill.sivokhin@vk.com>
2023-09-02 19:18:50 +0300

    BetterRest part 2

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kirill Sivokhin on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
